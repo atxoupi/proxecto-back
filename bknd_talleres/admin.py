10a7acd863c8ef0c7a1ce8f800e6f32f6a589e46
Andres Hermelo <andres.hermelo@gmail.com>
2025-11-02 17:48:41 +0100

    Register Model

--- bknd_talleres/admin.py
+++ bknd_talleres/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TallerService
+
+admin.site.register(TallerService)
+
--- bknd_talleres/models.py
+++ bknd_talleres/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Taller(models.Model):
+class TallerService(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
