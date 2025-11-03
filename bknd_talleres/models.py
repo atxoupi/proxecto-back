8c61dc3941d1377494cff8b32a6d2ff3d7b22696
Andres Hermelo <andres.hermelo@gmail.com>
2025-11-03 19:48:44 +0100

    Asign table str return

--- bknd_talleres/models.py
+++ bknd_talleres/models.py
@@ -9,3 +9,6 @@ class TallerService(models.Model):
     image = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
